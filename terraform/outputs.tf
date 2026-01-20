e2aa27632128786d6d4b04b3bd9ac27225e53c74
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 17:09:01 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "bindplane_ui_url" {
-  value = "http://${google_compute_instance.bindplane_vm.network_interface[0].access_config[0].nat_ip}:3001"
+  value = "http://${google_compute_instance.bindplane_control.network_interface[0].access_config[0].nat_ip}:3001"
 }
 
 output "gcs_bucket" {
@@ -9,3 +9,4 @@ output "gcs_bucket" {
 output "bindplane_vm_ip" {
   value = google_compute_instance.bindplane_control.network_interface[0].access_config[0].nat_ip
 }
+
