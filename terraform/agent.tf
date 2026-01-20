2a7e473fdb459a2e5baf677743d59711bff3c526
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 16:52:01 +0530

    Update agent.tf

--- terraform/agent.tf
+++ terraform/agent.tf
@@ -1,11 +1,11 @@
-resource "null_resource" "bindplane_agent" {
-  depends_on = [google_compute_instance.bindplane_vm]
+resource "null_resource" "install_bindplane_agent" {
+  depends_on = [google_compute_instance.bindplane_control]
 
   provisioner "remote-exec" {
     connection {
       type = "ssh"
       user = "ubuntu"
-      host = google_compute_instance.bindplane_vm.network_interface[0].access_config[0].nat_ip
+      host = google_compute_instance.bindplane_control.network_interface[0].access_config[0].nat_ip
     }
 
     inline = [
