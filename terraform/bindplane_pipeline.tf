29cf0275668d38cf554600a8c5d3b2bd67c94858
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 16:53:41 +0530

    Update bindplane_pipeline.tf

--- terraform/bindplane_pipeline.tf
+++ terraform/bindplane_pipeline.tf
@@ -1,27 +1,68 @@
+################################
+# Generate API key automatically
+################################
+resource "null_resource" "bindplane_api_key" {
+  depends_on = [null_resource.install_bindplane_agent]
+
+  provisioner "local-exec" {
+    command = <<EOT
+SERVER_IP=$(terraform output -raw bindplane_vm_ip)
+
+TOKEN=$(curl -s -X POST http://$SERVER_IP:3001/v1/session \
+  -H "Content-Type: application/json" \
+  -d '{"username":"admin","password":"password*test"}' | jq -r '.token')
+
+API_KEY=$(curl -s -X POST http://$SERVER_IP:3001/v1/api-keys \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{"name":"terraform-key"}' | jq -r '.key')
+
+echo "bindplane_api_key=\"$API_KEY\"" > bindplane.auto.tfvars
+EOT
+  }
+}
+
+################################
+# BindPlane Provider
+################################
 provider "bindplane" {
-  remote_url = "http://${google_compute_instance.bindplane_vm.network_interface[0].access_config[0].nat_ip}:3001"
+  remote_url = "http://${google_compute_instance.bindplane_control.network_interface[0].access_config[0].nat_ip}:3001"
   api_key    = var.bindplane_api_key
 }
 
-resource "bindplane_source" "journald" {
+################################
+# SOURCES
+################################
+resource "bindplane_source" "journald_logs" {
+  rollout = true
   name    = "journald-${random_id.suffix.hex}"
   type    = "journald"
-  rollout = true
 }
 
+################################
+# PROCESSOR
+################################
 resource "bindplane_processor" "batch" {
+  rollout = true
   name    = "batch-${random_id.suffix.hex}"
   type    = "batch"
-  rollout = true
 }
 
-resource "bindplane_configuration" "logs" {
+################################
+# PIPELINE CONFIGURATION
+################################
+resource "bindplane_configuration" "vm_logs_pipeline" {
+  rollout  = true
   name     = "vm-logs-${random_id.suffix.hex}"
   platform = "linux"
-  rollout  = true
+
+  labels = {
+    environment = "production"
+    managed_by  = "terraform"
+  }
 
   source {
-    name       = bindplane_source.journald.name
+    name       = bindplane_source.journald_logs.name
     processors = [bindplane_processor.batch.name]
   }
 
