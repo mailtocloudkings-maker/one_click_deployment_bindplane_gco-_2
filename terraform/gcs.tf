30492a977046be152710ae63cf548d18f4e6fccc
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 17:11:16 +0530

    Update gcs.tf

--- terraform/gcs.tf
+++ terraform/gcs.tf
@@ -1,5 +1,7 @@
 resource "google_storage_bucket" "logs" {
   name     = "bindplane-logs-${random_id.suffix.hex}"
   location = var.region
+
   uniform_bucket_level_access = true
+  force_destroy               = true
 }
