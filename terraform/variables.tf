96cf984e91426b9686bd8e7e6e3d2670be610111
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-20 17:44:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,9 +17,5 @@ variable "alert_email" {
   description = "Email for alert notifications"
   type        = string
 }
-variable "bindplane_api_key" {
-  description = "API key for BindPlane (auto-generated)"
-  type        = string
-  sensitive   = true
-}
+
 
